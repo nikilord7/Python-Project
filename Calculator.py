c6df14bce7c7eeb1d619b055691626cebe95f746
Nikita Nasirovs <2205823@talnet.nl>
2026-06-14 15:47:10 +0200

    Added comments

--- Calculator.py
+++ Calculator.py
@@ -1,14 +1,19 @@
+#Calculator class to perform basic functions
 class Calculator:
-
+    
+    #Addition function
     def Add(self, A, B):
         return A + B
-
+    
+    #Subtraction function
     def Subtract(self, A, B):
         return A - B
 
+    #Multiplication function
     def Multiply(self, A, B):
         return A * B
 
+    #Division function
     def Divide(self, A, B):
         if B == 0:
             raise ValueError("Delen door nul is niet toegestaan.")
--- History.py
+++ History.py
@@ -1,18 +1,22 @@
+#Import necessary libraries
 import json
 import os
 
-
+#Historymanager class to manage calculation history
 class HistoryManager:
 
     def __init__(self):
         self.Filename = "History.json"
 
+    #Save calculation to history
     def SaveCalculation(self, Calculation):
         History = self.LoadHistory()
         History.append(Calculation)
+        
         with open(self.Filename, "w") as File:
             json.dump(History, File, indent=4)
 
+    #Load history from file
     def LoadHistory(self):
         if not os.path.exists(self.Filename):
             return []
@@ -20,6 +24,7 @@ class HistoryManager:
         with open(self.Filename, "r") as File:
             return json.load(File)
 
+    #Clear your calculation history
     def ClearHistory(self):
         with open(self.Filename, "w") as File:
             json.dump([], File)
